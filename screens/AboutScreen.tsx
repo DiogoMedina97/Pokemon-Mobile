87388c69f0427d6438183fb63cc8b9a7d62f3361
DiogoMedina97 <diogo_medina97@hotmail.com>
2025-04-05 00:59:48 -0300

    feat(screen/AboutScreen): add about text

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-paper';
 
 export default function AboutScreen() {
   return (
-    <View>
-      <Text>AboutScreen</Text>
+    <View style={styles.container}>
+      <Text>App desenvolvido por Diogo Medina.</Text>
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+});
